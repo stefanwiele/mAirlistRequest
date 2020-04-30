132bba0169b7fc72abfe9b51e65eae9fc78f817c
Stefan <stefanwiele@users.noreply.github.com>
2020-04-30 22:59:33 +0200

    Create function to get search from mAirlist, return JSON

--- PHP/request.php
+++ PHP/request.php
@@ -3,11 +3,32 @@
 //get settings
 include('./inc/settings.inc.php');
 
-//get query string parameters
-$id = htmlspecialchars($_GET["id"]);
+function searchmAirlistDatabase($mlistIP,$mlistPort,$mlistUser,$mlistPassword,$search) {
 
-function searchmAirlistDatabase() {
+	//Contruct REST url
+	$apiRequestUrl = $mlistIP . ':' . $mlistPort . '/searchdatabase';
 
+	//Create new CURL instance
+	$client = curl_init($apiRequestUrl);
+
+	//Set Search
+	$data = array("search" => $search); 
+
+	//Create JSON to post
+ 	$dataCore = array("song" => $data);                                                                   
+	$data_string = json_encode($dataCore);   
+
+	//Set CURL options 
+	curl_setopt($client,CURLOPT_USERPWD, $mlistUser . ":" . $mlistPassword);
+ 	curl_setopt($client,CURLOPT_CUSTOMREQUEST, "POST");
+ 	curl_setopt($client, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+ 	curl_setopt($client, CURLOPT_POSTFIELDS, $data_string);
+	curl_setopt($client,CURLOPT_RETURNTRANSFER, true);	 
+
+	//Execute CURL
+	$response = curl_exec($client);
+
+	return $response;
 }
 
 function insertmAirlistRequest($mlistIP,$mlistPort,$mlistUser,$mlistPassword,$dbid) {
@@ -17,7 +38,7 @@ function insertmAirlistRequest($mlistIP,$mlistPort,$mlistUser,$mlistPassword,$db
 	//Create new CURL instance
 	$client = curl_init($apiRequestUrl);
 
-	//Get mAirlistDB ID
+	//Set mAirlistDB ID
 	$data = array("id" => $dbid); 
 
 	//Create JSON to post
@@ -34,31 +55,22 @@ function insertmAirlistRequest($mlistIP,$mlistPort,$mlistUser,$mlistPassword,$db
 	//Execute CURL
 	$response = curl_exec($client);
 
-	//Get Response
-	 $result = json_decode($response);
-
-	//Check Response 
- 	if ($result->success == 'true') {
-		return true;
- 	} else {
-		return false;
-	}
-
+	return $response;
 }
 
 
+//Check if request has an ID
+if (isset($_GET["id"]) && ($_GET["id"]) !="") {
 
-if (isset($_GET["id"]) && $_GET["id"]!="") {
+	$id = htmlspecialchars($_GET["id"]);
+	echo (insertmAirlistRequest($mairlistIP,$mairlistPort,$mairlistUser,$mairlistPassword,$id));
+	
+}
 
-	echo 'Trying to request song';
-
-	if (insertmAirlistRequest($mairlistIP,$mairlistPort,$mairlistUser,$mairlistPassword,$_GET['id'])) {
-		echo 'Request Added';
-	}
-	else {
-		echo 'Request Failed';
-	}
-
+elseif (isset($_GET["search"]) && ($_GET["search"]) !=""){
+	$search = htmlspecialchars($_GET["search"]);
+	echo (searchmAirlistDatabase($mairlistIP,$mairlistPort,$mairlistUser,$mairlistPassword,$search));
+	
 }
 
 ?>
